0fefd2b7e55cc1da991bf187dada265a6bea9ac3
David Aparicio <davidalejandro1223@hotmail.com>
2021-08-23 09:17:53 -0500

    fix modelo reserva foreign key con empleado

--- infrastructure/urls.py
+++ infrastructure/urls.py
@@ -5,14 +5,16 @@ from .views import (
     BranchOfficeListAPIView,
     BranchOfficeLoaderAPIView,
     AreaLoaderAPIView,
+    BookingStatusAPIView
 )
 
 router = DefaultRouter()
-router.register(r"infrastructure", BranchOfficeViewSet, basename="branchoffice")
+router.register(r"infrastructure/branch-office", BranchOfficeViewSet, basename="branchoffice")
 
 urlpatterns = [
     path("infrastructure/employee-branch-office", BranchOfficeListAPIView.as_view()),
     path("infrastructure/branch-office/loader", BranchOfficeLoaderAPIView.as_view()),
     path("infrastructure/areas/loader", AreaLoaderAPIView.as_view()),
+    path("infrastructure/branch-office/<int:branch_office_id>/booking-status", BookingStatusAPIView.as_view()),
 ]
 urlpatterns += router.urls
