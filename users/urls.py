3398cd921f0ce484f74ac196ee634eb09e313a36
David Aparicio <davidalejandro1223@hotmail.com>
2021-08-23 20:50:21 -0500

    endpoint info basica usuario

--- users/urls.py
+++ users/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken import views
-from .views import UserViewSet, ActivateUser
+from .views import UserViewSet, ActivateUser, UserDataAPIView
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet, basename='user')
 
 urlpatterns = [
     path('users/activate-account', ActivateUser.as_view()),
-    path('users/login', views.obtain_auth_token)
+    path('users/login', views.obtain_auth_token),
+    path('users/basic-data', UserDataAPIView.as_view()),
 ]
 urlpatterns+=router.urls
